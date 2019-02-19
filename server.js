f886693f25d5abbdff1eb67c2d1616e0f48a0658
Song <yousonghou@yahoo.com>
2019-02-19 00:43:42 +0000

    install body-parser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('It Working'));
 
