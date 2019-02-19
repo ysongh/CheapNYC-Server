215e9fe03a9bf1b7373113300068c8efe5279e88
Song <yousonghou@yahoo.com>
2019-02-19 00:24:46 +0000

    add a schema for item

---
+++ models/Item.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    company: {
+        type: String
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Item', itemSchema); 
