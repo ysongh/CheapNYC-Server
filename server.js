8f407da334e44dd0a86caed2b95cd4cf42058536
Song <yousonghou@yahoo.com>
2019-02-20 05:49:27 +0000

    install multer and set it up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const multer = require("multer");
 
 const db = require('./config/keys').mongoURI;
 
@@ -8,8 +9,22 @@ const itemRoutes = require("./routes/item");
 
 const app = express();
 
+const storage = multer.diskStorage({
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + file.originalname);
+    }
+});
+
+const imageFilter = (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return callback(new Error('Only image files are allowed!'), false);
+    }
+    callback(null, true);
+};
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(multer({ storage: storage, fileFilter: imageFilter }).single('image'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
