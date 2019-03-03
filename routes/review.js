6c8eb516f41277cce9fc8f3d3115863ab0ac2ba8
Song <yousonghou@yahoo.com>
2019-03-03 07:00:00 +0000

    add custom validation to check if the user enter a rating that is between 1 and 5

--- routes/review.js
+++ routes/review.js
@@ -13,7 +13,13 @@ router.post('/:itemId/reviews',
             .withMessage('Please enter your name that is least 2 characters long and not longer than 30 characters'),
         body('rating')
             .isNumeric()
-            .withMessage('Please enter a vaild number from 1 to 5'),
+            .withMessage('Please enter a vaild number from 1 to 5')
+            .custom((value, { req }) => {
+                if (value <= 0 || value > 5) {
+                    throw new Error('Please enter a number from 1 to 5');
+                }
+                return true;
+            }),
         body('text')
             .trim()
             .isLength({min: 2, max: 300})
