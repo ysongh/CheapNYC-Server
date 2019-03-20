2d463d006a43eee940b4b054fd2f2a501e4e4377
Song <yousonghou@yahoo.com>
2019-03-20 23:29:08 +0000

    add an image field to the user model

--- models/User.js
+++ models/User.js
@@ -15,6 +15,9 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String
+    },
     date: {
         type: Date,
         default: Date.now
