525b55d3ac675060835e323e33a89184e0c709ed
Song <yousonghou@yahoo.com>
2019-03-22 04:44:49 +0000

    add a schema for the user

---
+++ graphql/schema.js
@@ -0,0 +1,20 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(
+    `
+        type User{
+            _id: ID!
+            name: String!
+            email: String!
+            password: String!
+            image: String
+            date: String!
+        }
+        type Query{
+            users: [User!]!
+        }
+        schema {
+            query: Query
+        }
+    `
+); 
