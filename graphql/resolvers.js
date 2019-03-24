f01ff03b41e50fffc344b5d9c95d488a1bff479c
Song <yousonghou@yahoo.com>
2019-03-24 21:34:36 +0000

    make email and password null when getting all user data

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -5,7 +5,7 @@ module.exports = {
          return User.find()
             .then(users => {
                 return users.map(user => {
-                    return { ...user._doc, _id: user.id };
+                    return { ...user._doc, _id: user.id, email: null, password: null };
                 });
             })
             .catch(err => {
