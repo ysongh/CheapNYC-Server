ee2fc0691dadedd48cc135b5e36eca3504b29a49
Song <yousonghou@yahoo.com>
2019-03-25 00:33:54 +0000

    fix the problem with graphql

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
     next();
 });
 
