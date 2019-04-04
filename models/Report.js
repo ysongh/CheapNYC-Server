b5fc0db60b69e1a362e936b1e04442746efb5015
Song <yousonghou@yahoo.com>
2019-04-04 04:14:01 +0000

    fix naming error

--- models/Report.js
+++ models/Report.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
+const reportSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -22,4 +22,4 @@ const reviewSchema = new Schema({
     }
 });
 
-module.exports = mongoose.model('Review', reviewSchema);
+module.exports = mongoose.model('Report', reportSchema);
