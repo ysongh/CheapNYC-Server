f83b09f0012642398961f2641a2b9a7692aec4c4
Song <yousonghou@yahoo.com>
2019-04-26 02:33:40 +0000

    add a route for editing item

--- routes/item.js
+++ routes/item.js
@@ -38,6 +38,34 @@ router.post('/',
     
 router.get('/:itemId', itemController.findItemById);
 
+router.put('/',
+    [
+        body('name')
+            .trim()
+            .isLength({min: 2, max: 30})
+            .withMessage('Please enter the name of the item that is least 2 characters long and not longer than 30 characters'),
+        body('category')
+            .isLength({min: 2, max: 30})
+            .withMessage('Please select the Category'),
+        body('price')
+            .isNumeric()
+            .withMessage('Please enter a vaild price, ex - 1.99'),
+        body('location')
+            .isLength({min: 10, max: 70})
+            .withMessage('Please enter a vaild location'),
+        body('city')
+            .isLength({min: 2, max: 30})
+            .withMessage('Please select a city'),
+        body('description')
+            .trim()
+            .isLength({min: 5, max: 300})
+            .withMessage('Please enter a description that is least 5 characters long and not longer than 300 characters'),
+        body('company')
+            .isLength({min: 2, max: 50})
+            .withMessage('Please enter a name company that is least 2 characters long and not longer than 50 characters')
+    ],
+    itemController.createItem);
+
 router.put('/:itemId/like', passport.authenticate('jwt', {session: false}), itemController.likeItem);
 
 router.put('/:itemId/flag', passport.authenticate('jwt', {session: false}), itemController.flagItem);
