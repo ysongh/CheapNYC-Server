1506deb19d4b610482ef4de7bd6d1f142f22a791
Song <yousonghou@yahoo.com>
2019-04-26 20:37:17 +0000

    add more fields for user model

--- models/User.js
+++ models/User.js
@@ -21,6 +21,37 @@ const UserSchema = new Schema({
     image_id: {
         type: String
     },
+    title:{
+        type: String
+    },
+    point:{
+        type: Number,
+        default: 0
+    },
+    favorites: [
+        {
+            id:{
+                type: String,
+                required: true
+            },
+            name:{
+                type: String,
+                required: true
+            }
+        }
+    ],
+    listOfPosts: [
+        {
+            id:{
+                type: String,
+                required: true
+            },
+            name:{
+                type: String,
+                required: true
+            }
+        }
+    ],
     date: {
         type: Date,
         default: Date.now
